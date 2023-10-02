0fbb3ecbc958c88c3f6fa07f62b57bc93fee6b50
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-10-02 11:48:06 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.browserAction.disable()
+})
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=> {
+    if(changeInfo.status === "complete" && /^http/.test(tab.url)) {
+        chrome.scripting.executeScript({
+            target: {tabId},
+            files: ['./content.js']
+        }).then(()=> {
+            console.log("injected")
+        }).catch(error => console.log(error, "error in background script"))
+    }
+})
